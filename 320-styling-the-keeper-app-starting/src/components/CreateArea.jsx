5a2eee49a4d81c3bec2757dc04f7e11419297c68
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-04 10:55:23 +0200

    finish task 320

---
+++ 320-styling-the-keeper-app-starting/src/components/CreateArea.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [expanded, setExpanded] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    handleUnExpanded();
+    event.preventDefault();
+  }
+
+  const handleExpanded = () => {
+    setExpanded(true);
+  };
+
+  const handleUnExpanded = () => {
+    setExpanded(false);
+  };
+
+  return (
+    <div>
+      <form className="create-note">
+        {expanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={handleExpanded}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={expanded ? "3" : "1"}
+        />
+        <Zoom in={true}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
